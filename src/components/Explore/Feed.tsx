f20ed9e4b449584561cabd7cfc820ee46443073c
Matt <30867190+mattbrc@users.noreply.github.com>
2022-06-25 10:53:36 -0400

    added filter to only appId 'sparta'

--- src/components/Explore/Feed.tsx
+++ src/components/Explore/Feed.tsx
@@ -80,7 +80,8 @@ const Feed: FC<Props> = ({ feedType = 'TOP_COMMENTED' }) => {
             sortCriteria: feedType,
             cursor: pageInfo?.next,
             limit: 10,
-            noRandomize: feedType === 'LATEST'
+            noRandomize: feedType === 'LATEST',
+            sources: ['sparta']
           },
           reactionRequest: currentUser ? { profileId: currentUser?.id } : null
         }
